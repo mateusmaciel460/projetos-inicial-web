ad313683d5651f0e497d77aa66e4ba8948b84026
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-04-10 17:39:33 -0300

    adicionando listagem de elementos repetidos

---
+++ portfolio/js/listagemElementos.js
@@ -0,0 +1,44 @@
+let elementoLinksSociais = document.querySelector('#elemento-links-sociais');
+let elementoTopicos = document.querySelector('#elemento-topicos');
+
+function exibirLinksSociaisNaTela() {
+    let listaLinksSociais = [
+        { icone: 'map-marker', titulo: 'Fortaleza, Ceará' },
+        { icone: 'linkedin', titulo: 'in/mateusmaciel460' },
+        { icone: 'youtube', titulo: '@mateusmaciel460' }
+    ];    
+    
+    listaLinksSociais.forEach((rede) => {
+        elementoLinksSociais.innerHTML += `
+            <li class="conteudo__menu-item">
+                <i class="fa fa-${rede.icone}"></i>
+                ${rede.titulo}
+            </li>
+        `;
+    });
+}
+
+function exibirTopicos() {
+    let listaTopicos = [
+        { id: 'exibirVisaoGeral', icone: 'book', titulo: 'Visão geral' }, 
+        { id: 'exibirProjetos', icone: 'folder-open', titulo: 'Projetos' },
+        { id: 'exibirFormacao', icone: 'graduation-cap', titulo: 'Formação' },
+        { id: 'exibirCertificacao', icone: 'certificate', titulo: 'Certificações' },
+        { id: 'exibirContato', icone: 'phone', titulo: 'Contato' }
+    ];
+
+    listaTopicos.forEach((topico) => {
+        elementoTopicos.innerHTML += `
+            <li class="conteudo__menu-item modelo__horizontal">
+                <i class="fa fa-${topico.icone}"></i>
+                <a 
+                    href="#${topico.id}" 
+                    class="conteudo__link" 
+                    onclick="${topico.id}()">${topico.titulo}</a>
+            </li>
+        `;
+    })
+}
+
+exibirLinksSociaisNaTela();
+exibirTopicos();
